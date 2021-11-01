64169a2801a2753402c6013cc366e5a2bb02f586
Teal, D <teal.dillon@gmail.com>
2021-11-01 15:34:14 -0400

    Changed testing to not catch errors anymore, removed print statements from baby's first tests.

--- porchlight/door.py
+++ porchlight/door.py
@@ -5,6 +5,7 @@ import pdb
 class DoorError(Exception):
     pass
 
+
 class Door:
     '''The Door class is the primary 'model' or 'module' class of porchlight.
     It accesses functions and classes passed to it and parses it for metadata
@@ -42,7 +43,7 @@ class Door:
             self.model_type = 'function'
 
         elif inspect.ismethod(self.input_object):
-            sef.model_type = 'method'
+            self.model_type = 'method'
 
         else:
             raise DoorError(f"Object {self.input_object} not recognized as a "
@@ -52,6 +53,49 @@ class Door:
 
         # Get information using inspect.
         self.info = inspect.getmembers(self.input_object)
+
+        # If a class, get all possible attributes using string matching.
+        self._inspect_source_code()
+
+    def _inspect_source_code(self):
+        '''Inspects the source code of teh class or function definition using
+        inspect.getsource().
+        '''
+        source_code = inspect.getsource(self.input_object).split('\n')
+
+
+        # Get function return statements
+        if self.model_type == 'function':
+            for i, l in enumerate(source_code):
+                # Check for return statement
+                if 'return' == l.strip().split(' ')[0]:
+                    self.returns = [x.strip() for x in l.split('.')[1:]]
+
+        # Get class information not gathered by inspect
+        elif self.model_type == 'class':
+            for i, l in enumerate(source_code):
+                match l.split():
+                    case ['def', *definition]:
+                        # Check if this is really a method or a local
+                        # definition
+                        if 'self' not in ''.join(definition):
+                            continue
+
+                        else:
+                            joined_method = ''.join(definition)
+                            args = joined_method[1].split('self.')
+                            args = [arg.replace('):', '').strip() for arg in args]
+
+                    case []:
+                        # Empty line?
+                        print(f"Empty line found, skipping.")
+                        continue
+
+
+        else:
+            raise DoorError(f"Type not recognized (type is "
+                            f"{type(self.model_type)})."
+                            )
 
 def unit_test():
     '''Tests the definitions/classes/methods in this file.'''
@@ -72,19 +116,11 @@ def unit_test():
             return output_1, output_2
 
     # Test Door initialization
-    print(f"Testing door initialization... ", end='')
+    print(f"Testing door initialization... ")
 
-    try:
-        test_door = Door(TestClass, debug=False)
-
-    except:
-        print("failed!")
-        raise
+    test_door = Door(TestClass, debug=False)
 
     print(f"finished.")
-
-    for name, _type in test_door.info:
-        print(f"{name}:".ljust(15) + str(_type))
 
 
 if __name__ == "__main__":

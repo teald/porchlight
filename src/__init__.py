5d6c542ef8d94cc24a901f353bc5532cbea5a9be
Teal, D <teal.dillon@gmail.com>
2022-10-13 15:21:00 -0400

    Making Neighborhood and Door classes directly importable.

--- src/__init__.py
+++ src/__init__.py
@@ -5,3 +5,6 @@ import os
 logging.basicConfig(filename=f"{os.getcwd()}/porchlight.log")
 loggers = logging.getLogger(__name__)
 
+# Allow Neighborhood and Door to be directly importable.
+from neighborhood import Neighborhood
+from door import Door

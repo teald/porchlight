8391dc869fee1eccd5f5fc5c10e3a3e54c75d7a0
Teal, D <teal.dillon@gmail.com>
2022-10-26 13:28:01 -0700

    Updated example plots and temperature model.

--- examples/evolving_blackbody_spectrum.py
+++ examples/evolving_blackbody_spectrum.py
@@ -22,7 +22,8 @@ def blackbody(temperature: u.Quantity) -> Callable:
 
         term1 = 2 * const.h * const.c / wl ** 5
 
-        exponent = const.h * const.c / (wl * u.k_B * temperature)
+        exponent = const.h * const.c / (wl * const.k_B * temperature)
+
         term2 = np.power(np.exp(exponent) - 1, -1)
 
         return term1 * term2
@@ -40,10 +41,11 @@ def blackbody_peak(temperature: u.Quantity) -> u.Quantity:
 @porchlight.Door
 def temp_evolution(time: u.Quantity) -> u.Quantity:
     """Evolve the temperature over time as a logarithmic curve."""
-    k = 1e-6 * u.s ** -1
-    t_inflect = 1e6 * u.s
+    k = 2e-5 * u.s ** -1
+    t_inflect = 2e5 * u.s
+    t_start = 1000.0 * u.K
 
-    temperature = 1000.0 * u.K / (1 + np.exp(k * (time - t_inflect)))
+    temperature = t_start / (1 + np.exp(k * (time - t_inflect))) + 500.0 * u.K
 
     return temperature
 
@@ -55,15 +57,22 @@ def gen_spectrum(bb_fxn: Callable, wavelengths: u.Quantity):
     return spectrum
 
 
+@porchlight.Door
+def step_time_forward(time, dt=1e4 * u.s):
+    time = time + dt
+    return time
+
+
 def main():
     """Executes the example."""
     nbr = porchlight.Neighborhood()
 
     # Adding the Doors we've defined above
+    nbr.add_door(temp_evolution)
     nbr.add_door(blackbody)
     nbr.add_door(blackbody_peak)
-    nbr.add_door(temp_evolution)
     nbr.add_door(gen_spectrum)
+    nbr.add_door(step_time_forward)
 
     # List out the parameters in our system to the terminal.
     print("Neighborhood object contains the following parameters:")
@@ -76,10 +85,88 @@ def main():
         print(f"\t{dname}({argstr})")
 
     # Need to set the values for our required parameters. In this case, that's
-    # just temperature!
-    nbr.set_param("temperature", 500.0 * u.K)
+    # just wavelength grid and time!
+    nbr.set_param("time", 0.0 * u.s)
+    nbr.set_param(
+        "wavelengths", np.logspace(-1, np.log10(30), 10000) * u.micron
+    )
 
-    nbr.run_step()
+    # Let's run the model a number of times and collect data as the model
+    # evolves.
+    data = {}
+
+    N_STEPS = 50
+    for i in range(N_STEPS):
+        nbr.run_step()
+
+        # Since temperature, time, and spectrum are the only parameters really
+        # updating every step, just track those.
+        cur_temp = nbr.params["temperature"]
+        cur_time = nbr.params["time"]
+        cur_spectrum = nbr.params["spectrum"]
+
+        cur_data = {
+            "temperature": cur_temp.value,
+            "time": cur_time.value,
+            "cur_spectrum": cur_spectrum.value,
+        }
+
+        data[f"{cur_time.value:1.5e}"] = cur_data
+
+        # Print out info to the terminal during evolution.
+        print(
+            f"{i:4d}) Temperature: {cur_temp.value:6.1f} Kelvin "
+            f"|| Time: {cur_time.value:.3e} seconds"
+        )
+
+    # We now have our coupled blackbody/temperature evolution model ready, and
+    # we can use it as we'd like. Let's plot it to see if it worked out.
+    fig, ax = plt.subplots()
+
+    ax.set(
+        xscale="log",
+        xlabel="Wavelength (micron)",
+        ylabel="Radiance (W/m^2)",
+    )
+
+    times = list(data.keys())
+
+    N_SAMPLES = 10
+
+    cmap = plt.get_cmap("gist_rainbow")
+    colors = [cmap(i / N_SAMPLES) for i in range(N_SAMPLES)]
+
+    # Just reversing the colors to add hint of physicality.
+    colors = colors[::-1]
+
+    itimes = [i * len(times) // (N_SAMPLES - 1) for i in range(N_SAMPLES - 1)]
+    itimes += [len(times) - 1]
+
+    for i, itime in enumerate(itimes):
+        time = times[itime]
+
+        wl = nbr.params["wavelengths"].value.to(u.micron).value
+        spectrum = data[time]["cur_spectrum"].si.value
+
+        ax.plot(wl, spectrum, color=colors[i])
+
+    # Plot the temperature evolution with time for the run.
+    fig, ax = plt.subplots()
+
+    ax.set(xlabel="Time (seconds)", ylabel="Temperature (Kelvin)")
+
+    temperatures = [d["temperature"].value for d in data.values()]
+    times = [d["time"].value for d in data.values()]
+    sample_times = [times[i] for i in itimes]
+    sample_temps = [temperatures[i] for i in itimes]
+
+    ax.plot(times, temperatures, "k-")
+
+    for i, (time, temp) in enumerate(zip(sample_times, sample_temps)):
+        time = float(time)
+        ax.plot(time, temp, marker="o", color=colors[i])
+
+    plt.show()
 
 
 if __name__ == "__main__":

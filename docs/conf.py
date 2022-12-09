74071387fb3e1e257e9254e088fa33d4d4c7d8a9
Teal, D <teal.dillon@gmail.com>
2022-12-09 13:11:23 -0800

    Updating readthedocs requirements.

--- docs/conf.py
+++ docs/conf.py
@@ -67,4 +67,4 @@ html_theme_options = {"display_version": False}
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["source/_static"]
-html_css_files = ["porchlight.css"]
+# html_css_files = ["porchlight.css"]

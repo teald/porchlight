54bed7fce33cf802a459371646f458aba13dc6f0
Teal, D <teal.dillon@gmail.com>
2023-04-12 23:23:45 -0700

    Adding support for future migration to __init__.py
    v1.2.0 will migrate `Door`->`PorchlightAdapter` and
    `Neighborhood`->`PorchlightMediator`. For information about that change,
    you'll need to see the eventual issue ticket/pull request for that.

--- porchlight/__init__.py
+++ porchlight/__init__.py
@@ -13,6 +13,7 @@ Please see their respective documentation for details on usage, or check out
 the |porchlight| :doc:`quickstart` guide
 
 """
+import logging
 import os
 
 from .door import Door
@@ -20,7 +21,11 @@ from .neighborhood import Neighborhood
 from .param import Param
 
 
-# Initialize logging
-import logging
+# Aliases for migration to generic names, >=v1.2.0.
+PorchlightAdapter = Door
+PorchlightMediator = Neighborhood
 
+
+# Initialize logging with a NullHandler to let user decide on a handler if they
+# so choose.
 logging.getLogger(__name__).addHandler(logging.NullHandler())

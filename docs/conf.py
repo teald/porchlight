a446c86c53334dc04098b2eeed7d7fd3fdc6430e
Teal, D <teal.dillon@gmail.com>
2023-04-12 22:42:44 -0700

    Version bump to 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, D J Teal"
 author = "D J Teal"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.2"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
